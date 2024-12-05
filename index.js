80cc7aa45a619fd6b9299a4393089c68ebc6ef59
aiolah <yoyo31.music@gmail.com>
2024-12-06 00:46:08 +0100

    Affichage des explications

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ mapAnswers.set("foie", 3);
 mapAnswers.set("poumons", 3);
 mapAnswers.set("peau", 2);
 
+// Bonhomme
+let mapIdBonhomme = new Map();
+mapIdBonhomme.set("arteres", "heart");
+mapIdBonhomme.set("foie", "liver");
+mapIdBonhomme.set("poumons", "lugs");
+mapIdBonhomme.set("peau", "skin");
+
+// Explications
+let mapExplications = new Map();
+mapExplications.set("arteres", "")
+
+// Ordre questions
+let questions = ["arteres", "foie", "poumons", "peau"];
+
 // Question 1
-function letsPlay()
+function letsPlay(organe)
 {
     let buttons = document.querySelectorAll(".answer");
     let finish = false;
@@ -15,22 +29,48 @@ function letsPlay()
         button.addEventListener("click", () => {
             while(!finish)
             {
-                if(button.getAttribute("data-number") == mapAnswers.get("arteres"))
+                if(button.getAttribute("data-number") == mapAnswers.get(organe))
                 {
                     alert("Bien ouej mec !");
-                    localStorage.setItem("arteres", 25);
+                    localStorage.setItem(organe, 25);
+
+                    let idBonhomme = mapIdBonhomme.get(organe);
+                    document.querySelector("#" + idBonhomme).classList.add("text-success");
+
+                    let questionBadge = document.querySelector("#questionBadge");
+                    questionBadge.style.display = "none";
+
+                    let responseBadge = document.querySelector("#responseBadge");
+                    responseBadge.style.display = "";
+                    responseBadge.classList.add("text-bg-success");
+
+                    let answers = document.querySelector("#answers");
+                    answers.remove();
                 }
                 else
                 {
                     alert("Game over :(");
-                    localStorage.setItem("arteres", 0);
+                    localStorage.setItem(organe, 0);
+
+                    let idBonhomme = mapIdBonhomme.get(organe);
+                    document.querySelector("#" + idBonhomme).classList.add("text-danger");
+
+                    let questionBadge = document.querySelector("#questionBadge");
+                    questionBadge.style.display = "none";
+
+                    let responseBadge = document.querySelector("#responseBadge");
+                    responseBadge.style.display = "";
+                    responseBadge.classList.add("text-bg-danger");
+
+                    let answers = document.querySelector("#answers");
+                    answers.remove();
                 }
 
                 finish = true;
-                console.log(localStorage.getItem("arteres"));
+                console.log(localStorage.getItem(organe));
             }
         });
     });
 }
 
-letsPlay();
+letsPlay(questions[0]);
